bded7daf7dca92ca413ea683a4460ab90c93620c
ScarletDesigner <50834371+ScarletDesigner@users.noreply.github.com>
2019-06-13 00:12:44 +0200

    Removed global range variables

--- BisectionMethod.cpp
+++ BisectionMethod.cpp
@@ -3,61 +3,104 @@
 #include <conio.h>
 #include <cmath>
 using namespace std;
-const int numberOfFactors = 4;
-float factors[numberOfFactors];
-float begin, end;
-const float epsilon = 0.000001;
 
-float function(float factors[numberOfFactors], float x)
+class FileLoader
 {
-	float sum=0;
-	for(int i=0; i<numberOfFactors; i++)
-	{
-		int lastFactor = numberOfFactors - 1;
-		sum = sum * x + factors[lastFactor - i];
-	}
-	return sum;
-}
-float bisection(float factors[numberOfFactors], float a, float b)
-{	
-	float s = (a + b) / 2.0;
-	float Begin = function(factors,a);
-	float End = function(factors,b);
-	float Center = function(factors,s);
-	if (Center == 0)
-		return s;
+protected:
+	fstream file;
+	float start, end;
+	static const int numberOfFactors = 4;
+	float factors[numberOfFactors];
 
-	float precision = fabs(a - b);
-	if(epsilon > precision)
-		return a;
+public:
+	void loadFile(string fileName)
+	{
+		file.open(fileName.c_str());
+		checkIfFileExists();
+		loadBoundaryOfFunction();
+		loadFactors();
+		file.close();
+	}
+private:
+	void checkIfFileExists()
+	{
+		if(file.good() == false)
+		{
+			cout<<"Failed to load the file"<<endl;
+			exit(0);
+		}
+	}
 	
-	bool hasZeroInFirstHalf = (Begin * Center <= 0);
-	bool hasZeroInSeconHalf = (Center * End <= 0);
-	if(hasZeroInFirstHalf)
-		return bisection(factors, a, s);
-	if(hasZeroInSeconHalf)
-		return bisection(factors, s, b);
+	void loadBoundaryOfFunction()
+	{
+		file >> start;
+		file >> end;
+	}
 	
-}
+	void loadFactors()
+	{
+		for(int i=0; i<numberOfFactors; i++)
+		{
+			float factor;
+			file >> factor;
+			factors[i] = factor;
+		}
+	}
+};
+
+class ZeroFinder: public FileLoader
+{
+	static const float epsilon = 0.000001;
+	
+public:	
+	void printZeroOfFunction()
+	{
+		float precision = fabs(start - end);
+		float center;
+		while(epsilon < precision)
+		{
+			precision = fabs(start - end);
+			center = (start + end) / 2.0;
+			float startValue = getValue(start);
+			float endValue = getValue(end);
+			float centerValue = getValue(center);
+			if (centerValue == 0)
+			{
+				cout<<center<<endl;
+				exit(0);
+			}
+	
+			bool hasZeroInFirstHalf = (startValue * centerValue <= 0);
+			bool hasZeroInSecondHalf = (centerValue * endValue <= 0);
+			
+			if(hasZeroInFirstHalf)
+				end = center;
+				
+			if(hasZeroInSecondHalf)
+				start = center;
+		}
+		cout<<center<<endl;
+	}
+private:
+	float getValue(float x)
+	{
+		float sum=0;
+		for(int i=0; i<numberOfFactors; i++)
+		{
+			int lastFactor = numberOfFactors - 1;
+			sum = sum * x + factors[lastFactor - i];
+		}
+		return sum;
+	}
+	
+
+};
+
 int main(){
 	
-	fstream file("function.txt");
-	if(file.good()==false)
-	{
-		cout<<"Failed to load the file"<<endl;
-		exit(0);
-	}
-
-	file >> begin;
-	file >> end;
-	for(int i=0; i<numberOfFactors; i++)
-	{
-		float factor;
-		file >> factor;
-		factors[i] = factor;
-	}
-	file.close();
-	cout<<bisection(factors, begin, end);
-	getch();
+	ZeroFinder a;
+	a.loadFile("function.txt");
+	a.printZeroOfFunction();
+	
 	return 0;
 }
